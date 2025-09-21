3accc2b3bee13d0a76674136ea2db1a804acc858
aleister888 <pacoe1000@protonmail.com>
2025-09-21 16:49:13 +0200

    chore: disable nvim-treesitter for csv files

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -12,6 +12,12 @@ return {
 				enable = true,
 				-- Desactiva el highlighting para archivos grandes
 				disable = function(lang, buf)
+					-- Desactiva siempre para CSV
+					if lang == "csv" then
+						return true
+					end
+
+					-- Desactiva para archivos grandes
 					local max_filesize = 100 * 1024 -- 100 KB
 					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 					if ok and stats and stats.size > max_filesize then
