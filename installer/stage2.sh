fc9b6054170d6f4515e1d74210d4785e344811bd
aleister888 <pacoe1000@protonmail.com>
2025-11-29 23:08:05 +0100

    fix(install): ensure proper chaotic aur installation chore: fix windowrules

--- installer/stage2.sh
+++ installer/stage2.sh
@@ -101,10 +101,12 @@ repos_conf() {
 	EOF
 
 	# Añadir Chaotic AUR
-	pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-	pacman-key --lsign-key 3056513887B78AEB
-	pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-	pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	while ! pacman -Q chaotic-keyring chaotic-mirrorlist >/dev/null 2>&1; do
+		pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+		pacman-key --lsign-key 3056513887B78AEB
+		pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+		pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+	done
 
 	cat <<-EOF >>/etc/pacman.conf
 
